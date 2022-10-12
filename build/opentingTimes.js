69c3ab9ff96eb35c9b980788175ee6dfd7da510e
Thomas Solenthaler <tsolenthaler@gmail.com>
2022-10-12 10:35:59 +0200

    add opentingTimes.js

--- build/opentingTimes.js
+++ build/opentingTimes.js
@@ -1,11 +1,10 @@
-function myFunction(): void {
+"use strict";
+function myFunction() {
     let elements = document.getElementsByName("osm");
     console.log(document.getElementsByTagName('osm'));
     elements.forEach(element => {
         (console.log(element.nodeValue));
     });
 }
-
 console.log('Now');
-
-myFunction()
+myFunction();
